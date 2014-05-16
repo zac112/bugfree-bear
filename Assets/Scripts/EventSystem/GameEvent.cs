76235503496457d4a868a7bc9c59f98c3cbbe0d4
Erno Lokkila <eolokk@utu.fi>
2014-05-17 02:52:52 +0300

    Improved the event system. Events now work and can be registered to fire at specific times.

---
+++ Assets/Scripts/EventSystem/GameEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GameEvent : MonoBehaviour {
+
+	[SerializeField]
+	public GameTime startTime;
+	[SerializeField]
+	public GameTime endTime;
+
+	void Start(){
+		GameTimeManager.ReceiveNotificationAt(startTime, BeginEvent);
+	}
+
+	private void BeginEvent(){
+		Debug.Log("works");
+	}
+}
