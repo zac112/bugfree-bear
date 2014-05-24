81f23eb60b58dacfdced281d52f9cc0ca23e11f4
Erno Lokkila <eolokk@utu.fi>
2014-05-24 20:47:06 +0300

    Our first actual in-game event.

---
+++ Assets/Scripts/EventSystem/MoveEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MoveEvent : GameEvent {
+
+	protected override void BeginEvent ()
+	{
+		GetComponent<Seeker>().enabled = true;
+	}
+
+	protected override void EndEvent ()
+	{
+		GetComponent<Seeker>().enabled = false;
+	}
+}
