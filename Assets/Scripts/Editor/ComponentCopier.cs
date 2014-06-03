de31811c9de89a08fd7468818a2b97863e4d1ca4
vexe <xarxoohx@gmail.com>
2014-06-03 10:57:17 -0700

    New ComponentCopier tool

---
+++ Assets/Scripts/Editor/ComponentCopier.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+using EditorGUIFramework;
+using Vexe.EditorHelpers;
+using Vexe.RuntimeExtensions;
+using System.Linq;
+using UnityEditorInternal;
+
+public class ComponentCopier : EditorWindow
+{
+	private GLWrapper gui = new GLWrapper();
+	private GameObject source;
+	private GameObject destination;
+
+	[MenuItem("Vexe/ComponentCopier")]
+	public static void ShowMenu()
+	{
+		GetWindow<ComponentCopier>();
+	}
+
+	private void OnGUI()
+	{
+		gui.LabelWidthBlock(75f, () =>
+		{
+			gui.HorizontalBlock(() =>
+			{
+				gui.ObjectField("Source", source, newGo => source = newGo);
+				gui.MiniButton("S", "Set to current selection", () => source = Selection.activeGameObject);
+
+				gui.EnabledBlock(source != null, () =>
+					gui.MiniButton("C", "Clears source gameObject", MiniButtonStyle.Right, source.Clear)
+				);
+			});
+
+			gui.HorizontalBlock(() =>
+			{
+				gui.ObjectField("Destination", destination, newGo => destination = newGo);
+				gui.MiniButton("S", "Set to current selection", () => destination = Selection.activeGameObject);
+				gui.EnabledBlock(destination != null, () =>
+					gui.MiniButton("C", "Clears destination gameObject", MiniButtonStyle.Right, destination.Clear)
+				);
+			});
+		});
+
+		gui.FlexibleSpace();
+
+		gui.Button("Copy And Paste", () =>
+		{
+			var all = source.GetAllComponents();
+
+			// The first is the Transform component
+			ComponentUtility.CopyComponent(all[0]);
+			ComponentUtility.PasteComponentValues(destination.transform);
+
+			// We skip the first and copy/paste the rest as new components
+			foreach (var c in all.Skip(1))
+			{
+				ComponentUtility.CopyComponent(c);
+				ComponentUtility.PasteComponentAsNew(destination);
+			}
+		});
+	}
+}
