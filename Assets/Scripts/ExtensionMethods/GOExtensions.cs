24a47c1610872e22293cf4c6d237a8c9c33944cd
vexe <xarxoohx@gmail.com>
2014-06-03 14:25:29 -0700

    Took out GetInterface(s) from GOExtensions.
    They're already there in Vexe.RuntimeExtensions

--- Assets/Scripts/ExtensionMethods/GOExtensions.cs
+++ Assets/Scripts/ExtensionMethods/GOExtensions.cs
@@ -1,22 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public static class GOExtensions {
-
-	public static T GetInterface<T>(this GameObject go) where T : class{
-		Component c = go.GetComponent(typeof(T));
-		T result = c as T;
-		return result;
-	}
-
-	public static T[] GetInterfaces<T>(this GameObject go) where T: class{
-		Component[] components = go.GetComponents(typeof(T));
-		List<T> result = new List<T>();
-		
-		foreach(Component c in components){
-			result.Add(c as T);
-		}
-		
-		return result.ToArray();
-	}
+public static class GOExtensions
+{
 }
