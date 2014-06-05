755447873f70afa2583cf9bdb9af5adba914f341
vexe <xarxoohx@gmail.com>
2014-06-05 08:01:23 -0700

    NEW: Added a DefaultDrawer
    All it does is gui.PropertyField(property, text); - But it has the benefit of accepting a TWrapper which could be either a GLWrapper or GUIWrapper. This will fix most the ugly gui placement issues in BetterBehaviour since now I could just use GLWrapper everywhere.

---
+++ Assets/Vexe/ShowEmAll/Core/Editor/DrawMates/DefaultDrawer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using EditorGUIFramework;
+
+namespace ShowEmAll.DrawMates
+{
+	public class DefaultDrawer<TWrapper, TOption> : BaseDrawer<TWrapper, TOption>
+		where TWrapper : BaseWrapper<TOption>
+		where TOption : LayoutOption, new()
+	{
+		public SerializedProperty Property { get; set; }
+		public string DisplayText { get; set; }
+
+		public DefaultDrawer()
+		{
+		}
+
+		public DefaultDrawer(TWrapper gui)
+		{
+			this.gui = gui;
+		}
+
+		public DefaultDrawer(SerializedProperty sp, string displayText, TWrapper gui)
+			: this(gui)
+		{
+			Property = sp;
+			DisplayText = displayText;
+		}
+
+		public override void Draw()
+		{
+			gui.PropertyField(Property, DisplayText);
+		}
+	}
+}
