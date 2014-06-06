644472f4ad0c5392ea3c3539cc8a7c145b653e6b
vexe <xarxoohx@gmail.com>
2014-06-06 07:36:05 -0700

    Added a public color and private spriteRenderer properties to Tile

--- Assets/Scripts/Pathfinder/Tile.cs
+++ Assets/Scripts/Pathfinder/Tile.cs
@@ -1,11 +1,31 @@
 ﻿using UnityEngine;
+using System;
+using ShowEmAll;
 
-public class Tile : MonoBehaviour
+public class Tile : BetterBehaviour
 {
 	public bool isWalkable;
 
 	private void Start()
 	{
 		Nav.map.Register(transform, isWalkable);
+	}
+
+	private SpriteRenderer spriteRenderer
+	{
+		get
+		{
+			var r = GetComponent<SpriteRenderer>();
+			if (r == null)
+				throw new NullReferenceException("No SpriteRenderer attached to tile `" + name + "`");
+			return r;
+		}
+	}
+
+	[ShowProperty]
+	public Color color
+	{
+		get { return spriteRenderer.color; }
+		set { spriteRenderer.color = value; }
 	}
 }
