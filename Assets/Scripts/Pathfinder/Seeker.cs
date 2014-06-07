fbd577cb88c17050969e2cbc6597d9a04ee051f8
Erno Lokkila <eolokk@utu.fi>
2014-06-07 15:21:30 +0300

    Seeker should no longer get stuck between tiles.
    It now stays on the already computed path until the target moves far enough from its original location.

--- Assets/Scripts/Pathfinder/Seeker.cs
+++ Assets/Scripts/Pathfinder/Seeker.cs
@@ -4,27 +4,49 @@ using System.Collections.Generic;
 public class Seeker : MonoBehaviour
 {
 	public Transform targetPos;
+	private int[] currentPos = new int[2];
 	public Mover mover;
-	public List<Vector2> path = new List<Vector2>();
+	private List<Vector2> path = new List<Vector2> ();
+	private Vector3 rememberedTargetPos;
+	private List<Vector2> rememberedPath;
+	private int index;
 
-	void FixedUpdate()
+	public float minimumTargetMoveDistance = .4f;
+
+	void Awake ()
 	{
-		Nav.map.FindPath(transform, targetPos, path);
+		currentPos = new int[] {
+						(int)Mathf.Round (transform.position.x),
+						(int)Mathf.Round (transform.position.y)
+				};
+	}
 
-		//Vector2 to;
-		if (path.Count == 0)
-		{
-			//Debug.Log("path not found!");
+	void FixedUpdate ()
+	{
+		if ((rememberedTargetPos - targetPos.position).sqrMagnitude > minimumTargetMoveDistance) {
+			Nav.map.FindPath (transform, targetPos, path);
+			rememberedTargetPos = targetPos.position;
+			index = 0;
+		}
+
+		if (currentPos [0] != (int)Mathf.Round (transform.position.x) || currentPos [1] != (int)Mathf.Round (transform.position.y)) {
+			index = Mathf.Min (index + 1, path.Count - 1);
+			currentPos = new int[] {
+								(int)Mathf.Round (transform.position.x),
+								(int)Mathf.Round (transform.position.y)
+						};
+		}
+
+		if (path.Count == 0) {
 			return;
 		}
 
-		if (path.Count <= 1)
-		{
-			mover.Move(targetPos.position);
-		}
-		else
-		{
-			mover.Move(path[0]);
+		if (path.Count <= 1) {
+			mover.Move (targetPos.position);
+			index = 0;
+		} else {
+			mover.Move (path [index]);
+
 		}
 	}
 }
