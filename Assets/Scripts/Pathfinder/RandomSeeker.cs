f96135d2df5e71af606b644e0fb1076c5489176b
vexe <xarxoohx@gmail.com>
2014-06-08 02:43:38 -0700

    Added a RandomSeeker

---
+++ Assets/Scripts/Pathfinder/RandomSeeker.cs
@@ -0,0 +1,39 @@
+﻿using ShowEmAll;
+using UnityEngine;
+using System.Linq;
+using Vexe.RuntimeExtensions;
+
+public class RandomSeeker : Seeker
+{
+	[SerializeField, Required]
+	private GameObject tiles;
+
+	private void Start()
+	{
+		AssignNewTarget();
+	}
+
+	private void OnEnable()
+	{
+		onTargetReached += AssignNewTarget;
+	}
+
+	[ShowMethod]
+	public void AssignNewTarget()
+	{
+		target = tiles.GetComponentsInChildren<Tile>()
+					  .Where(t => t.isWalkable)
+					  .RandomElement().transform;
+	}
+
+	private void OnDrawGizmos()
+	{
+		if (target != null)
+			Gizmos.DrawLine(transform.position, target.position);
+	}
+
+	private void OnDisable()
+	{
+		onTargetReached -= AssignNewTarget;
+	}
+}
