0994ca094f36b24c2c307566ccdc14deada13251
Erno Lokkila <eolokk@utu.fi>
2014-06-14 18:11:49 +0300

    Selecting the scene to change is made easier.
    Selection is now done with the scene name instead of the int of the scene in the editor build settings list.

---
+++ Assets/Editor/SceneTriggerEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SceneTrigger))]
+public class SceneTriggerEditor : Editor {
+
+	private SceneTrigger _target;
+	private GUIContent[] sceneNames;
+	private int[] sceneIndexes;
+
+	void OnEnable(){
+		_target = (SceneTrigger)target;
+		int numberOfScenes = EditorBuildSettings.scenes.Length;
+		sceneNames = new GUIContent[numberOfScenes];
+		sceneIndexes = new int[numberOfScenes];
+		for (int i=0; i<numberOfScenes; i++)
+		{
+			sceneIndexes[i] = i;
+			string scenePath = EditorBuildSettings.scenes[i].path;
+			sceneNames[i] = new GUIContent(scenePath.Substring(scenePath.LastIndexOf("/")+1));
+		}
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		_target.sceneToLoad = EditorGUILayout.IntPopup(_target.sceneToLoad, sceneNames, sceneIndexes);
+	}
+
+}
