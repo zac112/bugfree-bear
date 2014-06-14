50a4d969e5013d1c1af3476ee6fd3edb99cbdf0b
Erno Lokkila <eolokk@utu.fi>
2014-06-14 14:42:09 +0300

    Added a forest scene and the ability to move to it.

---
+++ Assets/Scripts/SceneSwitching/SceneSwitcher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void OnLevelWasLoaded(){
+		transform.position = GameObject.Find ("Spawn").transform.position;
+	}
+}
---
+++ Assets/Scripts/SceneSwitching/SceneTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTrigger : MonoBehaviour {
+
+	public int sceneToLoad = 0;
+
+	void OnTriggerEnter2D(Collider2D other){
+		Application.LoadLevel (sceneToLoad);
+		Nav.instance.RecreateMap ();
+	}
+}
